325340b5c1ae38d8fb9f2ef07a957cb357c0453e
hakatonacclive <thatoneguyAryian_taha@proton.me>
2026-06-09 19:50:18 +0600

    Add files via upload

---
+++ newtestcode.py
@@ -0,0 +1,24 @@
+
+#যদি কেউ ব্যবসা শুরু করতে চান এবং তার কোনো নাম না থাকে, 
+#তাহলে তিনি এটি ব্যবহার করে নিজের ব্যবসার জন্য একটি নাম তৈরি করতে পারেন।
+
+print("welcome to the brand name generator\n")
+
+city = input("what was the name of the city that you grew up in?\n")
+
+PET = input("what is a name of an annimale that you like?\n")
+
+print("that name of your brand name is: " + city + " " + PET )
+
+tip = input("do you know what you need to to open a businness? (yes/no): ")
+
+if tip.lower() == "no":
+    print("here is what you need")
+    print("1. you will need a legal licenses to open one")
+    print("2. you will need and online store like shopify.com in case someone from an another country wants to buy something from you")
+    print("3. you will need marketing to get attention so people will buy from you")
+    print("4. this is the most important step, you need to belive in yourself")
+else:
+    print("Awesome you are ahead of the game! good luck bye bye :)")
+
+print("credits to rafad and raiyan")
